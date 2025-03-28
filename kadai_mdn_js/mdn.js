bd3053140e09e162e2f1f88cab02b3c1343977d2
rinck-donut <umkrn2120@gmail.com>
2025-03-29 02:37:49 +0900

    kadai_mdn_jsをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const now = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; 
+    const day = date.getDate();
+  
+    console.log(year + '年' + month + '月' + day + '日');
+  };
+  
+  now();
